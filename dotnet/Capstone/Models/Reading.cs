fa2af84c90413962e41fd3bbd4242b35db199a63
Andrew Hill <andrewhill@columbus.rr.com>
2023-04-10 11:41:40 -0400

    pulling

---
+++ dotnet/Capstone/Models/Reading.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Capstone.Models
+{
+    public class Reading
+    {
+        public int ReadingId { get; set; }
+        [Required, Range(0, int.MaxValue)]
+        public int UserId { get; set; }
+        [Required, Range(0, int.MaxValue)]
+        public int ProfileId { get; set; }
+        [Required, Range(0, int.MaxValue)]
+        public int BloodSugar { get; set; }
+        [Required]
+        public DateTime Time { get; set; }
+    }
+}
