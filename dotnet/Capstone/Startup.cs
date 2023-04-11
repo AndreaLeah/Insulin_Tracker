fb64d107458e7bf65ce39db0389cd57a22db44fb
Andrew Hill <andrewhill@columbus.rr.com>
2023-04-11 15:40:28 -0400

    Api fixes

--- dotnet/Capstone/Startup.cs
+++ dotnet/Capstone/Startup.cs
@@ -98,8 +98,8 @@ namespace Capstone
                 s.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
                 {
                     Version = "v1",
-                    Title = "Customize me in Startup.cs",
-                    Description = "Or just leave it and hope no hiring manager notices..."
+                    Title = "Insu-Trak",
+                    Description = "Track blood sugar and insulin"
                 });
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
