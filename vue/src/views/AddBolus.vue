6997107a873f7ee099e213bc840f0c9517010e86
Andrew Hill <andrewhill@columbus.rr.com>
2023-04-17 15:37:30 -0400

    Added bolus calucalation

--- vue/src/views/AddBolus.vue
+++ vue/src/views/AddBolus.vue
@@ -1,62 +1,115 @@
 <template>
-  <div>
-      <div>
-        <h2>Add new bolus</h2>
+    <div>
+        <h2>Calculate Bolus</h2>
         <form>
-            <label for="profileId"></label><input type="text" id='profileId' placeholder="Profile Id" v-model="newReading.profileId"/>
+            <profile-select/>
+            <label for="carbAmount"></label><input type="text" id='carbAmount' placeholder="Carb Amount" v-model="carbAmount"/>
             <label for="bloodSugar"></label><input type="text" id='bloodSugar' placeholder="Blood Sugar" v-model="newReading.bloodSugar"/>
-            <label for="carbAmount"></label><input type="text" id='carbAmount' placeholder="Carb Amount" v-model="newReading.carbs"/>
-            
-            <button @click.prevent="addBolus">Add</button>
+            <button @click.prevent="calculateBolus">Calculate</button>
         </form>
     </div>
-  </div>
 </template>
 
 <script>
 import ReadingService from '../services/ReadingsService.js';
+import ProfileSelect from '../components/ProfileSelect.vue';
 
 export default {
-data() {
+    data() {
         return {
             newReading: {
                 readingId: 0,
                 userId: 0,
                 profileId: '',
                 bloodSugar: '',
-                carbs: 0,
+                carbs: '',
                 time: ''
             },
-            newBolus: {
-                bolusId: 0,
-                readingId: 0,
-                bolusAmount: 0
-            }
+            carbAmount: '',
+            sugarToLow: false,
         }
     },
     methods: {
-        addBolus() {
+        calculateBolus() {
+           this.addReading();
+        },
+        checkReading() {
+            this.sugarToLow = false;
+            let profile = this.$store.state.userProfiles[this.$store.state.selectedProfileIndex - 1];
+            if (this.newReading.bloodSugar < profile.minWarningSugar) {
+                alert ("DANGEROUSLY LOW BLOOD SUGAR");
+                this.sugarToLow = true;
+            }
+            else if (this.newReading.bloodSugar < profile.minBloodSugar) {
+                alert("Low blood sugar");
+                this.sugarToLow = true;
+            }
+
+            if (this.newReading.bloodSugar > profile.maxWarningSugar) {
+                alert ("DANGEROUSLY HIGH BLOOD SUGAR")
+            }
+            else if (this.newReading.bloodSugar > profile.maxBloodSugar) {
+                alert("HIGH blood sugar");
+            }
+        },
+        addReading() {
             this.newReading.bloodSugar = +this.newReading.bloodSugar;
-            this.newReading.profileId = +this.newReading.profileId;
+            this.newReading.profileId = +this.$store.state.userProfiles[this.$store.state.selectedProfileIndex - 1].profileId;
+            this.newReading.carbs = +0;
 
             let d = new Date();
 
-            let dateTime = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate() + 
-                            'T' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + '.' + d.getMilliseconds();
+            let dateTime = 
+                d.getFullYear() + '-' + 
+                (d.getMonth()+1).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + '-' + 
+                d.getDate().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + 'T' + 
+                d.getHours() + ':' + 
+                d.getMinutes().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + ':' + 
+                d.getSeconds().toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + '.' + 
+                d.getMilliseconds();
 
             this.newReading.time = dateTime;
 
             ReadingService.addReading(this.newReading)
                 .then(response => {
-                    console.log(response);
-                    this.$router.push({name: 'Profile'});
+                    if (response.status === 200) {
+                        response;
+                        this.checkReading();
+                        console.log("Reading created");
+                        this.showBolus();
+                    }
+                    else {
+                        console.log("Error");
+                    }
                 })
                 .catch(error => {
                     console.log(error);
                 });
+        },
+        showBolus() {
+            let profile = this.$store.state.userProfiles[this.$store.state.selectedProfileIndex - 1]; 
+            if (profile != null)
+            {
+                if (this.sugarToLow) {
+                    alert("Sugar to low for a bolus");
+                } else {
+                    let carbBolus = +this.carbAmount / profile.carbRatio;
+                    let correctionBolus = 0; 
 
-            //ADD BOLUS TODO
+                    let bsMaxDifference = this.newReading.bloodSugar - profile.maxBloodSugar;
+                    if (bsMaxDifference > 0) {
+                        correctionBolus = bsMaxDifference / profile.correctionRatio; 
+                    }
+
+                    let totalBolus = carbBolus + correctionBolus;
+                    alert("Suggested Bolus: " + totalBolus);
+                    this.$router.push({name: 'Profile'});
+                }
+            }
         }
+    },
+    components: {
+        ProfileSelect
     }
 }
 </script>
