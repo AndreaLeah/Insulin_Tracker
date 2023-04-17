da70d0c29d8c77ac5afd70eccfea61e4edd8ce96
Angelia Thompson <thompson27thompson@gmail.com>
2023-04-17 11:42:33 -0400

    Implement frontend and backend files & database for activity log

---
+++ dotnet/Capstone/Models/Activity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Capstone.Models
+{
+    public class Activity
+    {
+        public int LogId { get; set; }
+        public int UserId { get; set; }
+        public string ActivityName { get; set; }
+        public DateTime Time { get; set; }
+
+    }
+}
