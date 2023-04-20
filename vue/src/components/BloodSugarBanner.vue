2acf2f0c8402f90838e6e02893574e706733257d
Andrew Hill <andrewhill@columbus.rr.com>
2023-04-20 11:07:42 -0400

    Changed banner

--- vue/src/components/BloodSugarBanner.vue
+++ vue/src/components/BloodSugarBanner.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
+    <div id="bannerParentDiv">
         <div class="banner" v-if="this.$store.state.isUserLowBloodSugar">
+            <img class="img1" src='../assets/exclamation.png'/>
             <div>LOW BLOOD SUGAR</div>
+            <img class="img2" src='../assets/exclamation.png'/>
             <button @click.prevent="closeBanner">x</button>
         </div>
         <div class="banner" v-if="this.$store.state.isUserHighBloodSugar">
+            <img class="img1" src='../assets/exclamation.png'/>
             <div>HIGH BLOOD SUGAR</div>
+            <img class="img2" src='../assets/exclamation.png'/>
             <button @click.prevent="closeBanner">x</button>
         </div>
     </div>
@@ -24,6 +28,10 @@ export default {
 
 <style scoped>
 
+#bannerParentDiv {
+    width: 100%
+}
+
 .banner {
     display: flex;
     justify-content: center;
@@ -31,24 +39,40 @@ export default {
 
     z-index: 10;
     height: 90px;
-    width: 100vw;
+    width: 100%;
 
-    background-color: rgb(221, 58, 58);
+    border: rgb(114, 13, 13) solid 2px;
+    background-color: rgb(231, 114, 114);
+}
+
+.banner > .img1 {
+    position: relative;
+    right: 230px;
+    width: 40px;
+    height: 40px;
+}
+
+.banner > .img2 {
+    position: relative;
+    left: 230px;
+    width: 40px;
+    height: 40px;
 }
 
 .banner > div {
     position: absolute;
     top: auto;
     right: auto;
-    color: white;
+    color: rgb(185, 22, 22);
     font-weight: bold;
+    font-size: 40px;
 }
 
 .banner > button {
     position: absolute;
     top: 30px;
     right: 10px;
-    font-size: 30px;
+    font-size: 40px;
     border: solid black 0px;
     background-color: rgba(0, 0, 0, 0);
 }
